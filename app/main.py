b1adc2349e05778923e3c79390bc7cdfae79fae3
Dean Horton <de_horton@hotmail.com>
2026-03-30 18:47:47 +0100

    Add scalar docs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
 from contextlib import asynccontextmanager
 
 from app.database.session import create_tables
@@ -13,3 +14,10 @@ app = FastAPI(lifespan=lifespan_handler)
 
 app.include_router(shipment.router)
 app.include_router(seller.router)
+
+@app.get("/scalar", include_in_schema=False)
+def get_scalar_docs():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API",
+    )
