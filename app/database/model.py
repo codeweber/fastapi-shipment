19f44a1f4e34464f5d7711461cb3193af1de84e9
Dean Horton <de_horton@hotmail.com>
2026-04-10 22:05:48 +0100

    Added created_at field Added delivery_partner to model with FK constraint

--- app/database/model.py
+++ app/database/model.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from sqlalchemy import ForeignKey
+from sqlalchemy.dialects import postgresql
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped
@@ -20,8 +23,12 @@ class Shipment(Base):
     estimated_delivery: Mapped[datetime]
     status: Mapped[ShipmentStatus]
     seller_id: Mapped[UUID] = mapped_column(ForeignKey("seller.id"))
+    delivery_partner_id: Mapped[UUID] = mapped_column(ForeignKey("delivery_partner.id"))
 
-    seller: Mapped["Seller"] = relationship(back_populates="shipments")
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+
+    seller: Mapped["Seller"] = relationship(back_populates="seller.shipments")
+    delivery_partner: Mapped["DeliveryPartner"] = relationship(back_populates="delivery_partner.shipments")
 
 class Seller(Base):
     __tablename__ = "seller"
@@ -30,4 +37,22 @@ class Seller(Base):
     email: Mapped[str]
     password_hash: Mapped[str]
 
-    shipments: Mapped["Shipment"] = relationship(back_populates="seller")
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+
+    shipments: Mapped[List["Shipment"]] = relationship(back_populates="seller")
+
+
+class DeliveryPartner(Base):
+    __tablename__ = "delivery_partner"
+    id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    name: Mapped[str]
+    email: Mapped[str]
+    password_hash: Mapped[str]
+
+    created_at: Mapped[datetime] = mapped_column(default=datetime.now)
+
+    max_shipment_capacity: Mapped[int]
+    zip_codes: Mapped[List[int]] = mapped_column(postgresql.ARRAY(postgresql.INTEGER))
+
+    shipments: Mapped[List["Shipment"]] = relationship(back_populates="delivery_partner")
+
